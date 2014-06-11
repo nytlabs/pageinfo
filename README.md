43722ea342a76e99d57ef69493e4a868cc6a657b
Deep Kapadia <deepkapadia@gmail.com>
2014-06-11 10:30:49 -0400

    significant changes. removed the Tornado dependancy. Added canonical. Library can now work as a stand alone module.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ pageinfo
 
 pageinfo is a simple module for extracting information from web pages. Currently, pageinfo will return the following from a url, where available:
 
+* Canonical
 * Title
 * Description
 * Favicon
@@ -16,35 +17,37 @@ pageinfo is a simple module for extracting information from web pages. Currently
 
 ##usage
 
-	import pageinfo
-	
-	pageinfo.get_meta('http://www.myurl.com', self)
-
+```python
+import pageinfo
+pageinfo.get_meta('http://www.myurl.com', self)
+```
 The first argument is the url you want to get info for and the second argument is the handler that is making the request (this assues pageinfo is being requested by a tornado server). The handler is required so that multiple requests can be made concurrently in a non-blocking fashion.
 
 The above code will return a dict with the available page information. Here's a sample response for `http://bits.blogs.nytimes.com/2013/11/20/a-gift-from-steve-jobs-returns-home`:
 
-	{
-    	"twitter": {    	
-        	"twitter:title": "A Gift From Steve Jobs Returns Home",
-       		"twitter:image": "http://graphics8.nytimes.com/images/2013/11/18/technology/bits-brilliant-jobs/bits-brilliant-jobs-thumbLarge.jpg",
-        	"twitter:description": "An Apple II that spent the last 33 years in Katmandu, Nepal, most of it packed away in a hospital basement there, was a rare symbol of the charity of Steven P. Jobs.",
-        	"twitter:url": "http://bits.blogs.nytimes.com/2013/11/20/a-gift-from-steve-jobs-returns-home/"
-    	},
-    	
-    	"favicon": "http://bits.blogs.nytimes.com/favicon.ico",
+```javascript
+    {   
+        "canonical": 
+        "twitter": {    
+            "twitter:title": "A Gift From Steve Jobs Returns Home",
+            "twitter:image": "http://graphics8.nytimes.com/images/2013/11/18/technology/bits-brilliant-jobs/bits-brilliant-jobs-thumbLarge.jpg",
+            "twitter:description": "An Apple II that spent the last 33 years in Katmandu, Nepal, most of it packed away in a hospital basement there, was a rare symbol of the charity of Steven P. Jobs.", 
+            "twitter:url": "http://bits.blogs.nytimes.com/2013/11/20/a-gift-from-steve-jobs-returns-home/"
+        },
     
-    	"facebook": {    	
-        	"og:url": "http://bits.blogs.nytimes.com/2013/11/20/a-gift-from-steve-jobs-returns-home/",
-        	"og:site_name": "Bits Blog",
-        	"og:type": "article",
-        	"og:description": "An Apple II that spent the last 33 years in Katmandu, Nepal, most of it packed away in a hospital basement there, was a rare symbol of the charity of Steven P. Jobs.",
-        	"og:title": "A Gift From Steve Jobs Returns Home",
-        	"og:image": "http://graphics8.nytimes.com/images/2013/11/18/technology/bits-brilliant-jobs/bits-brilliant-jobs-videoSixteenByNine600.jpg"
-    	},
+        "favicon": "http://bits.blogs.nytimes.com/favicon.ico",
     
-    	"description": "An Apple II that spent the last 33 years in Katmandu, Nepal, most of it packed away in a hospital basement there, was a rare symbol of the charity of Steven P. Jobs.",
+        "facebook": {    
+            "og:url": "http://bits.blogs.nytimes.com/2013/11/20/a-gift-from-steve-jobs-returns-home/",
+            "og:site_name": "Bits Blog",
+            "og:type": "article",
+            "og:description": "An Apple II that spent the last 33 years in Katmandu, Nepal, most of it packed away in a hospital basement there, was a rare symbol of the charity of Steven P. Jobs.", 
+            "og:title": "A Gift From Steve Jobs Returns Home",
+            "og:image": "http://graphics8.nytimes.com/images/2013/11/18/technology/bits-brilliant-jobs/bits-brilliant-jobs-videoSixteenByNine600.jpg"
+        },
     
-    	"title": "A Gift From Steve Jobs Returns Home - NYTimes.com"
-	}
-
+        "description": "An Apple II that spent the last 33 years in Katmandu, Nepal, most of it packed away in a hospital basement there, was a rare symbol of the charity of Steven P. Jobs.", 
+    
+        "title": "A Gift From Steve Jobs Returns Home - NYTimes.com"
+    }   
+```
--- pageinfo/pageinfo.py
+++ pageinfo/pageinfo.py
@@ -1,82 +1,88 @@
+import requests
 from bs4 import BeautifulSoup
 from HTMLParser import HTMLParseError
-import tornado.httpclient
 import json
 import re
 from urlparse import urlparse
 from urlparse import urljoin
-import functools
+
+#get canonical
+def get_canonical(url):
+  canonical_url = ""
+  response = requests.get(url)
+  if response.status_code == 200:
+    soup = BeautifulSoup(response.text)
+    canonical = soup.find("link", rel="canonical")
+    if canonical:
+      canonical_url = canonical['href']
+    else:
+      og_url = soup.find("meta", property="og:url")
+      canonical_url = og_url['content']
+  return canonical_url
 
 #get title, description, favicon, twitter card, facebook open graph data
-def get_meta(url, handler):
+def get_meta(url):
 
-  if url is not None:
-    callback = functools.partial(parse_html, handler=handler, url=url)
-    http_client = tornado.httpclient.AsyncHTTPClient() 
-    http_client.fetch(url, callback)
+    
+    data = {}
+    data["title"] = ""
+    data["description"] = None
+    data["favicon"] = None
+    data["facebook"] = {}
+    data["twitter"] = {}
 
-  else:
-    handler.write("something went wrong")
-    handler.finish()
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200: 
+            soup = BeautifulSoup(response.text)
 
-def parse_html(response, handler, url):
+            #get title
+            if soup.title.string:
+                data["title"] = soup.title.string
+            #get canonical
+            canonical = soup.find("link", rel="canonical")
+            if canonical:
+                data["canonical"] = canonical['href']
+            #get favicon
+            parsed_uri = urlparse( url )
+            if soup.find("link", rel="shortcut icon"):
+                icon_rel = soup.find("link", rel="shortcut icon")["href"]
+                icon_abs = urljoin( url, icon_rel )
+                data["favicon"] = icon_abs
+            else:
+                domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+                data["favicon"] = domain + 'favicon.ico'
 
-  data = {}
-  data["title"] = ""
-  data["description"] = None
-  data["favicon"] = None
-  data["facebook"] = {}
-  data["twitter"] = {}
+            #get description
+            if soup.find('meta', attrs={'name':'description'}):
+                data["description"] = soup.find('meta', attrs={'name':'description'})["content"]
 
-  try:
-    if response.code == 200: 
-        soup = BeautifulSoup(response.body)
+            #get facebook open graph data
+            if soup.findAll('meta', {"property":re.compile("^og")}):
+                for tag in soup.findAll('meta', {"property":re.compile("^og")}):
+                    tag_type = tag['property']
+                    data["facebook"][tag_type] = tag['content']
+                    if tag_type == "og:description" and data["description"] is None:
+                        data["description"] = tag["content"]
 
-        #get title
-        if soup.title.string:
-          data["title"] = soup.title.string
+            #get twitter card data
+            if soup.findAll('meta', attrs={'name':re.compile("^twitter")}):
+                for tag in soup.findAll('meta', attrs={'name':re.compile("^twitter")}):
+                    tag_type = tag['name']
+                    if 'content' in tag.attrs:
+                        data["twitter"][tag_type] = tag['content']
+                        if tag_type == "twitter:description" and data["description"] is None:
+                            data["description"] = tag["content"]
+            # make sure canonical exists, use og as backup
+            if not data['canonical'] or len(data['canonical']) == 0:
+                if data['facebook'].has_key('og:url'):
+                    data['canonical'] = data['facebook']['og:url']
+            if not data['canonical'] or len(data['canonical']) == 0:
+                data['canonical'] = url
 
-        #get favicon
-        parsed_uri = urlparse( url )
-        if soup.find("link", rel="shortcut icon"):
-          icon_rel = soup.find("link", rel="shortcut icon")["href"]
-          icon_abs = urljoin( url, icon_rel )
-          data["favicon"] = icon_abs
+
+            return json.dumps(data)
         else:
-          domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-          data["favicon"] = domain + 'favicon.ico'
-
-        #get description
-        if soup.find('meta', attrs={'name':'description'}):
-          data["description"] = soup.find('meta', attrs={'name':'description'})["content"]
-
-        #get facebook open graph data
-        if soup.findAll('meta', {"property":re.compile("^og")}):
-          for tag in soup.findAll('meta', {"property":re.compile("^og")}):
-            tag_type = tag['property']
-            data["facebook"][tag_type] = tag['content']
-            if tag_type == "og:description" and data["description"] is None:
-              data["description"] = tag["content"]
-
-        #get twitter card data
-        if soup.findAll('meta', attrs={'name':re.compile("^twitter")}):
-          for tag in soup.findAll('meta', attrs={'name':re.compile("^twitter")}):
-            tag_type = tag['name']
-            if 'content' in tag.attrs:
-              data["twitter"][tag_type] = tag['content']
-              if tag_type == "twitter:description" and data["description"] is None:
-                data["description"] = tag["content"]
-
-        handler.write(json.dumps(data))
-
-    else:
-      handler.write( "URL returned status %s" % response.code)
-
-  except HTMLParseError:
-    handler.write( "Error parsing page data" )
-
-  handler.finish()
-
-
-
-
+            return json.dumps({"canonical":url,"error":"URL returned status "+str(response.status_code)})
+    except HTMLParseError:
+        return json.dumps({"canonical":url,"error":"Error parsing page data"})
